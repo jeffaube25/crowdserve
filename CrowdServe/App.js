ddca7448a125bfa366b7c0c30c12872b302cf6b7
jeff <the8gig@gmail.com>
2024-03-19 13:12:25 -0400

    getting homepage set up

--- CrowdServe/App.js
+++ CrowdServe/App.js
@@ -3,18 +3,57 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.pageContainer}>
+      <View style={styles.flexContainer}>
+        <View style={styles.tile}>
+          <Text style={styles.text}>Crossroads</Text>
+        </View>
+        <View style={styles.tile}>
+          <Text style={styles.text}>Gracie's</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+
+      <View style={styles.flexContainer}>
+        <View style={styles.tile}>
+          <Text style={styles.text}>Campus Center</Text>
+        </View>
+        <View style={styles.tile}>
+          <Text style={styles.text}>Global Village</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  pageContainer: {
+    display: 'flex',
+    flexDirection: 'column',
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  flexContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 15,
+  },
+  tile: {
+    backgroundColor: '#808080',
+    width: 175,
+    height: 350,
+    borderRadius: '10%',
+    padding: 10,
+  },
+  text: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
 });
